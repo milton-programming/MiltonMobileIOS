2fe4d5130066027ebb66371b8f9feee7e0934567
jaronoff97 <jacobaronoff45@gmail.com>
2015-11-06 11:52:56 -0500

    Fixed 3d touch

---
+++ MiltonMobileIOS/Src/Util/DatePicker.swift
@@ -0,0 +1,9 @@
+//
+//  DatePicker.swift
+//  MiltonMobileIOS
+//
+//  Created by Jacob Aronoff on 11/5/15.
+//  Copyright © 2015 Milton Academy. All rights reserved.
+//
+
+import Foundation
---
+++ MiltonMobileStudent/MAInterfaceController.swift
@@ -0,0 +1,9 @@
+//
+//  MAInterfaceController.swift
+//  MiltonMobileIOS
+//
+//  Created by Jacob Aronoff on 11/6/15.
+//  Copyright © 2015 Milton Academy. All rights reserved.
+//
+
+import Foundation
